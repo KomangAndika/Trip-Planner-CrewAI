69c1dd3ef7d40fb10c827a0cd0dd81e07575db0e
KomangAndika <kwira.santosa@gmail.com>
2024-10-04 15:03:37 +0700

    adding-giggles

--- starter_template/agents.py
+++ starter_template/agents.py
@@ -4,6 +4,34 @@ from langchain.llms import OpenAI, Ollama
 from langchain_openai import ChatOpenAI
 
 
+"""
+Creating Agents Cheat Sheet:
+- Think like a boss. Work backwards from the goal and think which employee 
+    you need to hire to get the job done.
+- Define the Captain of the crew who orient the other agents towards the goal. 
+- Define which experts the captain needs to communicate with and delegate tasks to.
+    Build a top down structure of the crew.
+
+Goal:
+- Create a 7-day travel itinerary with detailed per-day plans,
+    including budget, packing suggestions, and safety tips.
+
+Captain/Manager/Boss:
+- Expert Travel Agent
+
+Employees/Experts to hire:
+- City Selection Expert 
+- Local Tour Guide
+
+
+Notes:
+- Agents should be results driven and have a clear goal in mind
+- Role is their job title
+- Goals should actionable
+- Backstory should be their resume
+"""
+
+
 # This is an example of how to define custom agents.
 # You can define as many agents as you want.
 # You can also define custom tasks in tasks.py
@@ -13,7 +41,7 @@ class CustomAgents:
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
         self.Ollama = Ollama(model="openhermes")
 
-    def agent_1_name(self):
+    def expert_travel_agent(self):
         return Agent(
             role="Define agent 1 role here",
             backstory=dedent(f"""Define agent 1 backstory here"""),
@@ -24,7 +52,18 @@ class CustomAgents:
             llm=self.OpenAIGPT35,
         )
 
-    def agent_2_name(self):
+    def city_selection_expert(self):
+        return Agent(
+            role="Define agent 2 role here",
+            backstory=dedent(f"""Define agent 2 backstory here"""),
+            goal=dedent(f"""Define agent 2 goal here"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def city_selection_expert(self):
         return Agent(
             role="Define agent 2 role here",
             backstory=dedent(f"""Define agent 2 backstory here"""),
